a6f6b6a68497a8bf98f220c639a8dc02dbf3ca27
Sinclair Target <sinclairtarget@gmail.com>
2014-05-24 18:22:11 +0800

    scaled up some sprites and started work on tank movement

---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveScript : MonoBehaviour 
+{
+	// designer variables
+	public int speed;
+	public Vector2 direction;
+
+	private Vector2 movement;
+
+	void start()
+	{
+		speed = 10;
+		direction = Vector2.zero;
+	}
+
+	// Update is called once per frame
+	void Update() 
+	{
+		movement = new Vector2(direction.x * speed, direction.y * speed);
+	}
+
+	void FixedUpdate()
+	{
+		rigidbody2D.velocity = movement;
+	}
+}
---
+++ Assets/Scripts/TankScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankScript : MonoBehaviour 
+{
+	private MoveScript moveScript;
+
+	public void Start()
+	{
+		moveScript = GetComponent<MoveScript>();
+	}
+
+	public void Update()
+	{
+		HandleInput();
+	}
+
+	private void HandleInput()
+	{
+		if (Input.GetKey("left"))
+		{
+			moveScript.direction = new Vector2(-1, 0);
+		}
+		else if (Input.GetKey("right"))
+		{
+			moveScript.direction = new Vector2(1,0);
+		}
+		else
+		{
+			moveScript.direction = Vector2.zero;
+		}
+	}
+}
